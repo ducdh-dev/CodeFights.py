3f5da57d554c5de6b4114130304402ec12e94e4c
DucDH-NTQ <duc.duong1@ntq-solution.com.vn>
2023-04-11 14:29:06 +0700

    feat: add math

---
+++ jsWay/practice/firstNotRepeatingCharacter.js
@@ -0,0 +1,34 @@
+function solution(s) {
+    let obj = {}
+    for (const item of s) {
+        if (obj[item]) {
+            obj[item]++
+        } else {
+            obj[item] = 1
+        }
+    }
+    for (const [k, v] of Object.entries(obj)) {
+        if(v === 1) return k;
+    }
+
+    return '_'
+}
+
+/**
+ * dùng indexOf và lastIndexOf
+ */
+function bestSolution(s) {
+
+    const arr = s.split("");
+
+    for(let i = 0; i < arr.length; i++){
+        if( arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])){
+            return arr[i]
+        }
+
+    }
+
+    return "_"
+}
+
+console.log(bestSolution('abacabad'))
